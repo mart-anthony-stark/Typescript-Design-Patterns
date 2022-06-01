07aa4fdca588d3b18b524e11041e5d47f1d122ea
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-01 21:12:33 +0800

    Add: decorator pattern

---
+++ decorator/decorator.ts
@@ -0,0 +1,14 @@
+class ModelS {
+  public description = "Model S";
+
+  public cost(): number {
+    return 73000;
+  }
+}
+class ModelX {
+  public description = "Model X";
+
+  public cost(): number {
+    return 77000;
+  }
+}
