8d46b816163b87b33129bc8b4833ff10cdf81ff0
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-01 21:25:54 +0800

    Add: decorator pattern

---
+++ decorator/decorator.js
@@ -0,0 +1,108 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.getDescription = function () {
+        return this.description;
+    };
+    ;
+    return Car;
+}());
+var CarOptions = /** @class */ (function (_super) {
+    __extends(CarOptions, _super);
+    function CarOptions() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return CarOptions;
+}(Car));
+// ------------------------------ Cars ------------------------------
+var ModelS = /** @class */ (function (_super) {
+    __extends(ModelS, _super);
+    function ModelS() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.description = "Model S";
+        return _this;
+    }
+    ModelS.prototype.cost = function () {
+        return 73000;
+    };
+    return ModelS;
+}(Car));
+var ModelX = /** @class */ (function (_super) {
+    __extends(ModelX, _super);
+    function ModelX() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.description = "Model X";
+        return _this;
+    }
+    ModelX.prototype.cost = function () {
+        return 77000;
+    };
+    return ModelX;
+}(Car));
+// ------------------------------ Options ------------------------------
+var EnhancedAutoPilot = /** @class */ (function (_super) {
+    __extends(EnhancedAutoPilot, _super);
+    function EnhancedAutoPilot(car) {
+        var _this = _super.call(this) || this;
+        _this.decoratedCar = car;
+        return _this;
+    }
+    EnhancedAutoPilot.prototype.getDescription = function () {
+        return this.decoratedCar.getDescription() + ', Enhanced Autopilot';
+    };
+    EnhancedAutoPilot.prototype.cost = function () {
+        return this.decoratedCar.cost() + 5000;
+    };
+    return EnhancedAutoPilot;
+}(CarOptions));
+var SmartAirSuspension = /** @class */ (function (_super) {
+    __extends(SmartAirSuspension, _super);
+    function SmartAirSuspension(car) {
+        var _this = _super.call(this) || this;
+        _this.decoratedCar = car;
+        return _this;
+    }
+    SmartAirSuspension.prototype.getDescription = function () {
+        return this.decoratedCar.getDescription() + ', Smart Air Suspension';
+    };
+    SmartAirSuspension.prototype.cost = function () {
+        return this.decoratedCar.cost() + 2500;
+    };
+    return SmartAirSuspension;
+}(CarOptions));
+var RearFacingSeats = /** @class */ (function (_super) {
+    __extends(RearFacingSeats, _super);
+    function RearFacingSeats(car) {
+        var _this = _super.call(this) || this;
+        _this.decoratedCar = car;
+        return _this;
+    }
+    RearFacingSeats.prototype.getDescription = function () {
+        return this.decoratedCar.getDescription() + ', Rear Facing Seats';
+    };
+    RearFacingSeats.prototype.cost = function () {
+        return this.decoratedCar.cost() + 4000;
+    };
+    return RearFacingSeats;
+}(CarOptions));
+// ------------------------------ Configure our Tesla! ------------------------------
+var myTesla = new ModelS();
+myTesla = new SmartAirSuspension(myTesla);
+myTesla = new RearFacingSeats(myTesla);
+console.log(myTesla.cost());
+console.log(myTesla.getDescription());
--- decorator/decorator.ts
+++ decorator/decorator.ts
@@ -1,14 +1,85 @@
-class ModelS {
-  public description = "Model S";
+abstract class Car{
+    public description: string;
 
-  public cost(): number {
-    return 73000;
-  }
-}
-class ModelX {
-  public description = "Model X";
+    public getDescription() : string{
+        return this.description;
+    };
 
-  public cost(): number {
-    return 77000;
-  }
+    public abstract cost(): number;
 }
+
+abstract class CarOptions extends Car{
+    decoratedCar: Car;
+    public abstract getDescription(): string;
+}
+
+// ------------------------------ Cars ------------------------------
+class ModelS extends Car{
+    public description = "Model S";
+
+    public cost(): number {
+        return 73000;
+    }
+}
+
+class ModelX extends Car {
+    public description = "Model X";
+
+    public cost(): number {
+        return 77000
+    }
+}
+
+// ------------------------------ Options ------------------------------
+class EnhancedAutoPilot extends CarOptions {
+    constructor(car: Car) {
+        super();
+        this.decoratedCar = car;
+    }
+
+    public getDescription(): string {
+        return this.decoratedCar.getDescription() + ', Enhanced Autopilot';
+    }
+
+    public cost(): number {
+        return this.decoratedCar.cost() + 5000;
+    }
+}
+
+class SmartAirSuspension extends CarOptions {
+    constructor(car: Car) {
+        super();
+        this.decoratedCar = car;
+    }
+
+    public getDescription(): string {
+        return this.decoratedCar.getDescription() + ', Smart Air Suspension';
+    }
+
+    public cost(): number {
+        return this.decoratedCar.cost() + 2500;
+    }
+}
+
+class RearFacingSeats extends CarOptions {
+    constructor(car: Car) {
+        super();
+        this.decoratedCar = car;
+    }
+
+    public getDescription(): string {
+        return this.decoratedCar.getDescription() + ', Rear Facing Seats';
+    }
+
+    public cost(): number {
+        return this.decoratedCar.cost() + 4000;
+    }
+}
+
+// ------------------------------ Configure our Tesla! ------------------------------
+let myTesla = new ModelS();
+myTesla = new SmartAirSuspension(myTesla);
+myTesla = new RearFacingSeats(myTesla);
+
+console.log(myTesla.cost());
+console.log(myTesla.getDescription());
